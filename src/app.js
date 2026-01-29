23019df5db9b628a2c5e05225aae235ca6b344c9
Erick de los Reyes <delosreyeserick90@gmail.com>
2026-01-29 16:09:37 +0000

    First version completed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,64 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  //Declare variables
+  const topIcon = document.querySelector("#cardTop");
+  const bottomIcon = document.querySelector("#cardBottom");
+  const cardValue = document.querySelector("#cardValue");
+  const button = document.querySelector("#cardButton");
+
+  const icons = ["♥︎", "♣", "♦", "♠"];
+  const values = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+
+  //Generate a card function
+  let randomItem = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+
+  const mixCards = () => {
+    const icon = randomItem(icons);
+    const value = randomItem(values);
+    topIcon.innerHTML = icon;
+    bottomIcon.innerHTML = icon;
+    cardValue.innerHTML = value;
+
+    const redIcon = icon === "♥︎" || icon === "♦";
+    if (redIcon) {
+      topIcon.classList.add("text-danger");
+      bottomIcon.classList.add("text-danger")
+      topIcon.classList.remove("text-dark");
+      bottomIcon.classList.remove("text-dark")
+    }
+    else {
+        topIcon.classList.add("text-dark");
+        bottomIcon.classList.add("text-dark");
+      }
+  };
+
+  
+
+  //Load card in every window load
+  mixCards();
+  //Listener for the generate card button
+  button.addEventListener("click", mixCards);
+  console.log("Hello Erick from the console!");
 };
